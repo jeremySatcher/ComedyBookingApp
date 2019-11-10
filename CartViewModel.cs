1f0711088a48f9c41645e63b687c90b10980de6e
bridgetmott <54859715+bridgetmott@users.noreply.github.com>
2019-11-10 15:29:38 -0600

    Add files via upload

---
+++ CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IList<Event> EventList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
---
+++ HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Comedian> ComedianList { get; set; }
+        public IEnumerable<Event> EventList { get; set; }
+    }
+}
