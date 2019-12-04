833322b49e019ed0aa4f4ff1de3e8c69dbeaa268
bridgetmott <54859715+bridgetmott@users.noreply.github.com>
2019-12-04 13:35:00 -0600

    Update Booker.cs

--- ComedyBookingApp/Booker.cs
+++ ComedyBookingApp/Booker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MySecureApplication
+namespace ComedyBookingApp
 {
     public static class Booker
     {
