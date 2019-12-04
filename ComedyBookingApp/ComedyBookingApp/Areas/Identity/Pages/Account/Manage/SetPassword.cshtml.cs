338d7cb6670be57b8d255b605d712f40a8737a15
bridgetmott <54859715+bridgetmott@users.noreply.github.com>
2019-12-04 14:05:51 -0600

    Update SetPassword.cshtml.cs

--- ComedyBookingApp/ComedyBookingApp/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
+++ ComedyBookingApp/ComedyBookingApp/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MySecureApplication.Areas.Identity.Pages.Account.Manage
+namespace ComedyBookingApp.Areas.Identity.Pages.Account.Manage
 {
     public class SetPasswordModel : PageModel
     {
