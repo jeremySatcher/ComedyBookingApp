b2c8df47595b36052b085d2581c6e73acbd9f2c3
bridgetmott <54859715+bridgetmott@users.noreply.github.com>
2019-12-04 13:33:47 -0600

    Add files via upload

---
+++ ComedyBookingApp/Booker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySecureApplication
+{
+    public static class Booker
+    {
+        public const string BookerRole = "Booker";
+    }
+}
